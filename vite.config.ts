ffa91d974699e4bb0bccd7ba32c5d4e9b414d3b5
farazzshaikh <farazzshaikh@gmail.com>
2026-08-26 22:33:41 +0400

    Add GH Pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import PackageJson from "./package.json" with { type: "json" };
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: `/${PackageJson.name}/`,
 });
